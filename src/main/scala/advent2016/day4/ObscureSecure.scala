a68359b4dcc203d9dd220854277c17c94c6dbe6d
Campbell <cammorr@gmail.com>
2016-12-05 17:16:06 +0200

    Day4 Part 2 done

--- src/main/scala/advent2016/day4/ObscureSecure.scala
+++ src/main/scala/advent2016/day4/ObscureSecure.scala
@@ -19,17 +19,17 @@ object ObscureSecure {
   def part2(inputFile: String) = {
     val result = FileReader.readFileByLine(inputFile)
       .map(expand)
-      // TODO: Decrypt, then Filter
-//      .map(decrypt)
-      .toList
+      .map(decrypt)
+      .filter(p => p._1.equals("northpole object storage"))
+      .next
 
-    println(s"Part2: Sector ID where North Pole objects are stored: ${result.head._2}")
+    println(s"Part2: Sector ID where North Pole objects are stored: ${result._1} sectionId ${result._2}")
   }
 
   def expand(input: String): (String, Int, String) = {
     val suffix = input.substring(input.lastIndexOf('-') + 1)
 
-    val encoding = input.substring(0, input.length - suffix.length).replaceAll("-", "")
+    val encoding = input.substring(0, input.length - suffix.length - 1)
     val sectorId = suffix.substring(0, suffix.indexOf('[')).toInt
     val checksum = suffix.substring(suffix.indexOf('[') + 1, suffix.length - 1)
 
@@ -37,7 +37,7 @@ object ObscureSecure {
   }
 
   def reorganizeExpanded(expanded: (String, Int, String)): (String, Int, String) = {
-    (orderUniqueCharsByOccurrence(expanded._1), expanded._2, expanded._3)
+    (orderUniqueCharsByOccurrence(expanded._1.replaceAll("-", "")), expanded._2, expanded._3)
   }
 
   def orderUniqueCharsByOccurrence(input: String): String = {
@@ -50,11 +50,13 @@ object ObscureSecure {
 
   def isValidSector(encoding: (String, Int, String)): Boolean = encoding._1.take(5).equals(encoding._3)
 
-//  def decryptCharacter(encrypted: Char, sectorId: Int): Char = encrypted match {
-//    // TODO
-//  }
-//
-//  def decrypt(encrypted: (String, Int, String)): (String, Int, String) = {
-//    // TODO
-//  }
+  def shiftChar(encrypted: Char, shiftTimes: Int): Char = encrypted match {
+    case '-' => ' '
+    case char => (((char % 'a') + shiftTimes) % 26 + 'a').toChar
+  }
+
+  def decrypt(encrypted: (String, Int, String)): (String, Int, String) = {
+    (encrypted._1.toList.map(shiftChar(_, encrypted._2)).foldLeft("")((acc, value) => s"$acc$value"),
+      encrypted._2, encrypted._3)
+  }
 }
--- src/test/scala/advent2016/day4/ObscureSecureTest.scala
+++ src/test/scala/advent2016/day4/ObscureSecureTest.scala
@@ -9,13 +9,13 @@ class ObscureSecureTest extends FunSuite {
 
   test("A line must be expanded into its three components") {
     assert(ObscureSecure.expand("a-1[a]") === ("a", 1, "a"))
-    assert(ObscureSecure.expand("ab-cd-12[kebab]") === ("abcd", 12, "kebab"))
-    assert(ObscureSecure.expand("lkjdf-a-lkalk-sdjf-812[ollo]") === ("lkjdfalkalksdjf", 812, "ollo"))
+    assert(ObscureSecure.expand("ab-cd-12[kebab]") === ("ab-cd", 12, "kebab"))
+    assert(ObscureSecure.expand("lkjdf-a-lkalk-sdjf-812[ollo]") === ("lkjdf-a-lkalk-sdjf", 812, "ollo"))
   }
 
   test("Given an expanded tuple, sort encoding section by occurrence") {
-    assert(ObscureSecure.reorganizeExpanded(("cbba", 2, "a")) === ("bac", 2, "a"))
-    assert(ObscureSecure.reorganizeExpanded(("cba", 654, "cba")) === ("abc", 654, "cba"))
+    assert(ObscureSecure.reorganizeExpanded(("cb-ba", 2, "a")) === ("bac", 2, "a"))
+    assert(ObscureSecure.reorganizeExpanded(("c-b-a", 654, "cba")) === ("abc", 654, "cba"))
   }
 
   test("Given a string, order all unique characters by occurrence") {
@@ -30,5 +30,17 @@ class ObscureSecureTest extends FunSuite {
     assert(ObscureSecure.isValidSector(("ab", 1, "ba")) === false)
     assert(ObscureSecure.isValidSector(("abc", 1, "abc")) === true)
     assert(ObscureSecure.isValidSector(("abcdefglhad", 1, "abcde")) === true)
+  }
+
+  test("Given a character and a shift count, shift the character that many characters along the alphabet, wrapping at 'z'") {
+    assert(ObscureSecure.shiftChar('-', 123) === ' ')
+    assert(ObscureSecure.shiftChar('a', 5) === 'f')
+    assert(ObscureSecure.shiftChar('a', 26) === 'a')
+    assert(ObscureSecure.shiftChar('a', 29) === 'd')
+  }
+
+  test("Given an encoded tuple, decrypt the encoded value using the sectorId") {
+    assert(ObscureSecure.decrypt("xyz", 3, "") === ("abc", 3, ""))
+    assert(ObscureSecure.decrypt("axeeh-phkew", 33, "") === ("hello world", 33, ""))
   }
 }
